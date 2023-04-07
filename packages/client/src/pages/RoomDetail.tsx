5a8a0f05710d3d7f8e4e8da114c84d43df4b3a4b
jhlee <wisdom_13_@naver.com>
2023-04-07 14:12:45 +0900

    메세지 API 연동

--- packages/client/src/pages/RoomDetail.tsx
+++ packages/client/src/pages/RoomDetail.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import styled from "@emotion/styled";
 import { css, Global } from "@emotion/react";
+import MessageList from "../components/RoomDetail/MessageList";
+import InputChat from "../components/RoomDetail/InputChat";
+import TopNavigation from "../components/RoomDetail/TopNavigation";
+import { useParams } from "react-router-dom";
+import { useQuery, useMutation } from "react-query";
+import { fetchMyprofile } from "../apis/userApi";
+import { fetchChatRoomDetail } from "../apis/roomApi";
+import { fetchChatMessageList, sendChatMessage } from "../apis/chatApi";
+import { IChat, IProfile, IRoom } from "../types";
+import { AxiosResponse, AxiosError } from "axios";
+import SentMessage from "../components/RoomDetail/SentMessage";
+import ReceiveMessage from "../components/RoomDetail/ReceiveMessage";
 
-const Base = styled.div``;
+const Base = styled.div`
+  position: relative;
+`;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 64px;
+  align-items: center;
+  padding: 0 24px;
+`;
 
 const globalStyle = css`
   body {
@@ -13,12 +33,59 @@ const globalStyle = css`
 `
 
 const RoomDetail: React.FC = () => {
+  const { roomId } = useParams<string>();
+
+  const { data: profileData } = useQuery<AxiosResponse<IProfile>, AxiosError>(
+    'fetchMyProfile',
+    fetchMyprofile
+  );
+
+  const { data: chatRoomDetailData } = useQuery<AxiosResponse<IRoom>, AxiosError>(
+    ['fetchChatRoomDetail', roomId],
+    () => fetchChatRoomDetail(roomId as string)
+  );
+
+  const { data: chatListData } = useQuery<AxiosResponse<Array<IChat>>>(
+    ['fetchChatMessageList', roomId],
+    () => fetchChatMessageList(roomId as string)
+  );
+
+  const mutation = useMutation('sendChatMessage', (content: string) => sendChatMessage(roomId as string, content));
+
+  const handleSend = (content: string) => {
+    if (content.length) {
+      mutation.mutate(content);
+    }
+  }
+
   return (
     <Base>
       <Global styles={globalStyle} />
+
+      {chatRoomDetailData && (
+        <TopNavigation title={chatRoomDetailData?.data.user.username} />
+      )}
       <Container>
-
-
+        <MessageList>
+          {
+            chatListData?.data.map(message =>
+              message.senderId === profileData?.data.userId ? (
+                <SentMessage
+                  senderId={message.senderId}
+                  content={message.content}
+                  timestamp={message.createdAt}
+                />
+              ) : (
+                <ReceiveMessage
+                  receiver={message.user?.username}
+                  senderId={message.senderId}
+                  content={message.content}
+                  timestamp={message.createdAt}
+                />
+              ))
+          }
+        </MessageList>
+        <InputChat onClick={handleSend} />
       </Container>
     </Base>
   )
